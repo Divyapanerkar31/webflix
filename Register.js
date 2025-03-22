3dbbdade1beae2edd48d1e8d87367d49236203ca
Divyapanerkar31 <divyapanerkar31@gmail.com>
2025-03-23 00:03:36 +0530

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBeDtC1sdo8fifC3nvoKb3DJwniA5OV70M",
+    authDomain: "webflixproject.firebaseapp.com",
+    projectId: "webflixproject",
+    storageBucket: "webflixproject.firebasestorage.app",
+    messagingSenderId: "718300622612",
+    appId: "1:718300622612:web:3b76e5993d27bda87adc01"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
